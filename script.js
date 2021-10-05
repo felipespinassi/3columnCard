b64edc6e393a5c548f58ef1345334834190cd430
Felipe Augusto <77703754+felipespinassi@users.noreply.github.com>
2021-10-04 21:00:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const button_dark = document.querySelector ('[data-button]')
 const body = document.querySelector ('body')
 
+
 button_dark.addEventListener('click', function() {
     body.classList.toggle("dark_mode")
+
+
+    const title = document.querySelectorAll( ".color_dark_text")
+    Array.prototype.forEach.call(title, function(title){
+        title.classList.toggle('color_dark')
+
+    })
+
+    const saberMais = document.querySelectorAll (".button_saibaMais")
+        Array.prototype.forEach.call(saberMais, function(saberMais){
+            saberMais.classList.toggle('button_dark_saberMais')
+        })
+
+
+
 })
+
 
